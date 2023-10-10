07d82f262a01e07bea1375c515da07d8142f7096
risethesummer <honhatlinhmail@gmail.com>
2023-10-10 17:22:12 +0700

    add web socket adapter

---
+++ Realtime/Controllers/Transporters/Impl/WebSocketAdapter.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.WebSockets;
+using Realtime.Controllers.Transporters.Interfaces;
+
+namespace Realtime.Controllers.Transporters.Impl;
+
+public class WebSocketAdapter : ISocket
+{
+    private readonly WebSocket _socket;
+    private static WebSocketManager? _webSocketManager;
+    public WebSocketAdapter(WebSocket socket)
+    {
+        _socket = socket;
+    }
+    public static void SetManager(WebSocketManager webSocketManager)
+    {
+        _webSocketManager = webSocketManager;
+    }
+    public async ValueTask<ISocket> AcceptAsync(CancellationToken cancellationToken)
+    {
+        var accepted = await _webSocketManager.AcceptWebSocketAsync();
+        return new WebSocketAdapter(accepted);
+    }
+
+    public async ValueTask<int> ReceiveAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+    {
+        var res = await _socket.ReceiveAsync(buffer, cancellationToken);
+        return res.Count;
+    }
+
+    public async ValueTask<int> SendAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _socket.SendAsync(buffer, WebSocketMessageType.Binary, true, cancellationToken);
+            return buffer.Length;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return 0;
+        }
+    }
+    public ValueTask DisconnectAsync(bool reuse, CancellationToken cancellationToken) =>
+        new(_socket.CloseAsync(WebSocketCloseStatus.Empty, string.Empty, cancellationToken));
+
+    public EndPoint? EndPoint => null;
+
+    public void Dispose()
+    {
+        _socket.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,19 +4,22 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Realtime.Data;
 
-Console.WriteLine("Hello world");
-// using IHost host = Host.CreateDefaultBuilder(args)
-//     .ConfigureServices(services =>
-//     {
-//         services.Add(ServiceDescriptor.Singleton<>());
-//         services.AddSingleton<MyClass2>();        
-//         services.AddSingleton<MyClass>();        
-//     })
-//     .Build();
-//
+Console.Title = "Hello world";
+var host = new WebHostBuilder()
+    .UseKestrel()
+    .Build();     
+using var app = Host.CreateDefaultBuilder(args)
+    
+     .ConfigureServices(services =>
+     {
+         // services.Add(ServiceDescriptor.Singleton<>());
+         // services.AddSingleton<MyClass2>();        
+         // services.AddSingleton<MyClass>();        
+     })
+     .Build();
 // var myClass = host.Services.GetService<MyClass>();
 // await myClass!.DoStuff();
 //
-// await host.RunAsync();
+await host.RunAsync();
 
 // Generate all code depending all 2 classes
